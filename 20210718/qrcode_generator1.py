4075e177f7b776c598a19828a327a64eba531dcd
Seungeun_Song <lilybad88@gmail.com>
2021-07-21 19:04:35 +0900

    something deleted

--- 20210718/qrcode_generator1.py
+++ 20210718/qrcode_generator1.py
@@ -1,6 +1,9 @@
 import pyqrcode
+import pyshorteners
 
 msg = input("QR코드로 변환할 메시지를 입력해주세요 : ")
 qrcode = pyqrcode.create(msg)
 qrcode.png("my_qrcode1.png", scale=15)
+url = 'www.naver.com'
+
 print("QR코드가 생성되었습니다.")
