1b9f1c0c0429a34bb2c0cc45a3c1baf7fa984d6d
Seungeun_Song <lilybad88@gmail.com>
2021-08-15 13:48:27 +0900

    weather_list_API.py

---
+++ 20210801/weather_list_API.py
@@ -0,0 +1,24 @@
+from urllib import request
+import json
+from prettytable import PrettyTable
+
+URL = "http://api.openweathermap.org/data/2.5/weather?q={city name}&lang={lang}&appid={API key}"
+
+CITY = ["Seoul", "Daegu", "Busan"]
+LANG = "kr"
+API_KEY = "d64dc55592f29a938741d0a4e3aefe76"
+
+dataTable = PrettyTable()
+
+for city in CITY:
+    with request.urlopen(URL.replace("{city name}", city).replace("{lang}", LANG).replace("{API key}", API_KEY)) as response:
+        result_json = response.read()
+    parsed_json = json.loads(result_json)
+    print(parsed_json["weather"])
+
+
+    dataTable.field_names = parsed_json["weather"][0].keys()
+
+    for i in parsed_json["weather"]:
+        dataTable.add_row(i.values())
+print(dataTable)
