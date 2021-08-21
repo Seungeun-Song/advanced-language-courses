8af54f379cacd11a877a4d6c9b5256285ef2377f
Seungeun_Song <lilybad88@gmail.com>
2021-08-21 18:48:59 +0900

    Add file_005.py

---
+++ 20210807/file_005.py
@@ -0,0 +1,8 @@
+import 
+
+if __name__ =="__main__":
+    with open("example/recipe-book.pdf", "r") as f:
+        reader = #PyPDF3.PdfFileReader(f)
+        print(reader)
+
+        https://youtu.be/PMQay1Rz50I?t=4997
